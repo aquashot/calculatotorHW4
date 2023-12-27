997086eaa7a9cf606c052681be1819572218b0d5
Илон <vlasovvdm@gmail.com>
2023-12-27 14:47:55 +0400

    !

---
+++ src/main/java/com/calculator/demcalculator/DemcalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.calculator.demcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemcalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemcalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calculator/demcalculator/DemcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculator.demcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemcalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
